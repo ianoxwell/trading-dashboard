608368a7b55072e0535d636fb2bf1ee836ccea06
ianoxwell <ianoxwell@gmail.com>
2025-08-12 18:56:37 +1000

    feat: reduced number of browsers and tweaked test settings for performance

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,15 +4,16 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  timeout: 180000,
   testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env['CI'],
   /* Retry on CI only */
-  retries: process.env['CI'] ? 2 : 0,
+  retries: process.env['CI'] ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env['CI'] ? 1 : undefined,
+  workers: process.env['CI'] ? 4 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -22,40 +23,40 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    
+
     /* Take screenshot on failure */
-    screenshot: 'only-on-failure',
-    
+    screenshot: 'only-on-failure'
+
     /* Record video on failure */
-    video: 'retain-on-failure',
+    // video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     {
       name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
+      use: { ...devices['Pixel 5'] }
     },
     {
       name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+      use: { ...devices['iPhone 12'] }
+    }
 
     /* Test against branded browsers. */
     // {
@@ -73,6 +74,6 @@ export default defineConfig({
     command: 'ng serve --configuration=test',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env['CI'],
-    timeout: 120 * 1000,
-  },
+    timeout: 120 * 1000
+  }
 });
