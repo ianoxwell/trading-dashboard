de81fa224aa7503880e924634f9cb65150fb5619
ianoxwell <ianoxwell@gmail.com>
2025-08-12 10:35:36 +1000

    feat: trade confirmation modal

--- stake-dashboard/src/app/pages/detail/detail.module.ts
+++ stake-dashboard/src/app/pages/detail/detail.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CoreModule } from '@core/core.module';
 import { IonicModule } from '@ionic/angular';
 import { DetailRoutingModule } from './detail-routing.module';
 import { DetailComponent } from './detail.component';
+import { TradeConfirmationModalComponent } from './trade-confirmation-modal.component';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, DetailRoutingModule],
-  declarations: [DetailComponent]
+  imports: [CommonModule, FormsModule, IonicModule, DetailRoutingModule, CoreModule],
+  declarations: [DetailComponent, TradeConfirmationModalComponent]
 })
 export class DetailModule {}
